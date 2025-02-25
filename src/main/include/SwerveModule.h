fba94619c1a1e5cb62e38590243759ded64b8c89
Drive Laptop (Probably Max) <122837652+Roboctopi@users.noreply.github.com>
2025-02-24 21:15:26 -0800

    Swerve fully configured on bench - may need to adjust values after testing on ground

--- src/main/include/SwerveModule.h
+++ src/main/include/SwerveModule.h
@@ -61,21 +61,17 @@ private:
    int m_turningEncoderOffset;
 
    frc::PIDController m_drivePIDController{
-       0.1, //previously 1.0 on old swerve
-       0.0,
+       0.05, //previously 1.0 on 2024 swerve
+       0.0, 
        0.0};
 
    frc::ProfiledPIDController<units::radians> m_turningPIDController{
-       // 8.0,  //[Something] too violent, 5 too slow
-       // 0.0,
-       // 0.001,
-       // Values above are good for all 4 swerve module turn motors
-       5.0, //previously 8.0 on old swerve
+       5.0, //previously 8.0 on 2024 swerve
        0.0,
-       0.0, //previously .001 on old swerve
+       0.0, //previously .001 on 2024 swerve
        {kModuleMaxAngularVelocity, kModuleMaxAngularAcceleration}};
-                                                                    // was 1.0
-   frc::SimpleMotorFeedforward<units::meters> m_driveFeedforward{0.1_V, 1_V / 1_mps};
-   frc::SimpleMotorFeedforward<units::radians> m_turnFeedforward{0.1_V /*was 0.1*/, 0.025_V /* was 0.025*/ / 1_rad_per_s};
+                                                                    
+   frc::SimpleMotorFeedforward<units::meters> m_driveFeedforward{6.0_V, 1_V / 1_mps}; //modify this 6.0 to go faster. May require PID retuning
+   frc::SimpleMotorFeedforward<units::radians> m_turnFeedforward{0.1_V, 0.025_V / 1_rad_per_s};
 };
 // originally .5_V on line 72 made 0.025_V and 3_V on line 70 made 1_V
